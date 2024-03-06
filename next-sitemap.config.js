7c9cabbb50b5b22cc9dc063547866028bfa4d18b
unknown <andrey.monroo@gmail.com>
2024-03-06 14:00:25 +0300

    add sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://xn--b1afanhwgieieg.online",
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+};
