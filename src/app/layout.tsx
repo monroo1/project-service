77e550695c917ebe66393e4d4fc7589fbd3b4a3d
monroo <andrey.monroo@gmail.com>
2025-05-28 15:04:20 +0300

    feat: change metrik

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { FirstScreen } from "@/widgets/firstScreen";
 import { Footer } from "@/widgets/footer";
 import Providers from "./providers";
 import "./styles/main.scss";
-import Script from "next/script";
 
 export const metadata: Metadata = {
     title: "Проект сервис | Кухни",
@@ -26,21 +25,6 @@ export default function RootLayout({
                 <FirstScreen />
                 <Providers>{children}</Providers>
                 <div id="modal-root"></div>
-                <Script id="yandex-metrika" strategy="afterInteractive">
-                    {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                        m[i].l=1*new Date();
-                        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-                        ym(96671347, "init", {
-                                clickmap:true,
-                                trackLinks:true,
-                                accurateTrackBounce:true,
-                                webvisor:true
-                        });
-                    `}
-                </Script>
                 <Footer />
             </body>
         </html>
--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -17,6 +17,7 @@ import {
     getRouteStagesWork,
 } from "@/shared/const/router";
 import { useRouter } from "next/navigation";
+import Script from "next/script";
 
 interface FooterProps {
     className?: string;
@@ -68,6 +69,30 @@ export const Footer = memo((props: FooterProps) => {
             <HStack className={cls.bottom} justify="center">
                 <Text text="© 2025 Проект сервис" />
             </HStack>
+            <Script id="yandex-metrika" strategy="afterInteractive">
+                    {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                        m[i].l=1*new Date();
+                        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                        ym(102220715, "init", {
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true,
+                            webvisor:true
+                        });
+                    `}
+            </Script>
+            <noscript>
+                    <div>
+                        <img
+                            src="https://mc.yandex.ru/watch/102220715"
+                            style={{ position: 'absolute', left: '-9999px' }}
+                            alt=""
+                        />
+                    </div>
+                </noscript>
         </footer>
     );
 });
